15f33258c132714351bb8a64216b98ffa56591ea
carlynorama <carlynorama@gmail.com>
2022-07-31 10:52:50 -0700

    Initial Commit

---
+++ DataViewer/DataViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataViewerApp.swift
+//  DataViewer
+//
+//  Created by Labtanza on 7/31/22.
+//
+
+import SwiftUI
+
+@main
+struct DataViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
