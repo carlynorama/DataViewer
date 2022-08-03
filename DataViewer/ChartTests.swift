6458a76b9648db98092b2663ec59f58d63f94566
carlynorama <carlynorama@gmail.com>
2022-08-03 14:29:10 -0700

    checking on charts behavior //result downloading new beta

--- DataViewer/ChartTests.swift
+++ DataViewer/ChartTests.swift
@@ -1,68 +1,71 @@
-////
-////  ChartTests.swift
-////  DataViewer
-////
-////  Created by Labtanza on 8/3/22.
-////
 //
+//  ChartTests.swift
+//  DataViewer
 //
-//extension Date {
-//    static func random(in range: Range<Date>) -> Date {
-//        Date(
-//            timeIntervalSinceNow: .random(
-//                in: range.lowerBound.timeIntervalSinceNow...range.upperBound.timeIntervalSinceNow
-//            )
-//        )
-//    }
-//}
+//  Created by Labtanza on 8/3/22.
 //
-//import SwiftUI
-//import Charts
-//
-//struct ChartTests: View {
-//    struct ProfitOverTime {
-//        var date: Date
-//        var profit: Double
-//    }
-//
-//    static func dataBuilder() {
-//        var days:[Date] = []
-//        var dateComponets =
-//        let year = Calendar.current.component(.year, from: Date())
-//        for i in 1...12 {
-//            let date = Date.
-//        }
-//    }
-//    
-//    let departmentAProfit: [ProfitOverTime] = [] // ...
-//    let departmentBProfit: [ProfitOverTime] = [] // ...
-//
-//    var body: some View {
-//        Chart {
-//            ForEach(departmentAProfit) {
-//                LineMark(
-//                    x: .value("Date", $0.date),
-//                    y: .value("Profit A", $0.profit)
-//                )
-//                .foregroundStyle(.blue)
-//            }
-//            ForEach(departmentBProfit) {
-//                LineMark(
-//                    x: .value("Date", $0.date),
-//                    y: .value("Profit B", $0.profit)
-//                )
-//                .foregroundStyle(.green)
-//            }
-//            RuleMark(
-//                y: .value("Threshold", 500.0)
-//            )
-//            .foregroundStyle(.red)
-//        }
-//    }
-//}
-//
-//struct ChartTests_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ChartTests()
-//    }
-//}
+
+import SwiftUI
+import Charts
+
+struct ChartTests: View {
+    struct ProfitOverTime {
+        var date: Date
+        var profit: Double
+    }
+
+    static func months() -> [Date] {
+        var days:[Date] = []
+        var dateComponets = DateComponents()
+        let year = Calendar.current.component(.year, from: Date())
+        dateComponets.year = year
+        dateComponets.day = 1
+        for i in 1...12 {
+            dateComponets.month = i
+            if let date = Calendar.current.date(from: dateComponets) {
+                days.append(date)
+            }
+        }
+    }
+    
+    static func dataBuilder() -> [ProfitOverTime]{
+        var data:[ProfitOverTime] = []
+        for month in months() {
+            let new = ProfitOverTime(date: month, profit: Double.random(in: 200...600))
+            data.append(new)
+        }
+        return data
+    }
+    
+    let departmentAProfit: [ProfitOverTime] = Self.dataBuilder()
+    let departmentBProfit: [ProfitOverTime] = Self.dataBuilder()
+
+    var body: some View {
+        Chart {
+            ForEach(departmentAProfit) {
+                LineMark(
+                    x: .value("Date", $0.date),
+                    y: .value("Profit A", $0.profit)
+                )
+                .foregroundStyle(.blue)
+            }
+            ForEach(departmentBProfit) {
+                LineMark(
+                    x: .value("Date", $0.date),
+                    y: .value("Profit B", $0.profit)
+                )
+                .foregroundStyle(.green)
+            }
+            RuleMark(
+                y: .value("Threshold", 500.0)
+            )
+            .foregroundStyle(.red)
+        }
+    }
+}
+
+struct ChartTests_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartTests()
+    }
+}
